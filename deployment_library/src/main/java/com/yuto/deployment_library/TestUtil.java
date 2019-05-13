29e881f8bf2d4949be412fcb04de515642fec99b
lvhongzhen <lvhongzhen@qudao.com>
2019-05-13 18:52:25 +0800

    发布sdk1.0.4

--- deployment_library/src/main/java/com/yuto/deployment_library/TestUtil.java
+++ deployment_library/src/main/java/com/yuto/deployment_library/TestUtil.java
@@ -7,4 +7,8 @@ public class TestUtil {
     public static void test() {
         Log.i("lib","我是TestUtil的test方法");
     }
+
+    public static void test104() {
+        Log.i("lib","我是新增方法");
+    }
 }
