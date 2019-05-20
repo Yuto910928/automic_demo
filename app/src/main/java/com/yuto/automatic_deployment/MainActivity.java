d71f3439ce5868d6d1e61ddd92818460b66e9307
lvhongzhen <lvhongzhen@qudao.com>
2019-05-20 16:28:56 +0800

    提交修改

--- app/src/main/java/com/yuto/automatic_deployment/MainActivity.java
+++ app/src/main/java/com/yuto/automatic_deployment/MainActivity.java
@@ -1,9 +1,7 @@
 package com.yuto.automatic_deployment;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.yuto.deployment_library.TestUtil;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TestUtil.test();
+
     }
 }
